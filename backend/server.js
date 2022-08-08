177fad64eceefe499e71da73be54bd5f0432286a
dorgrosman <ddorgrosman@gmail.com>
2022-08-08 13:34:30 +0300

    .env

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,18 @@ const connectDB = require('./config/db');
 connectDB()
 
 const port = process.env.PORT || 5000;
+
+
 const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use('/api/users', require('./routes/userRoutes'))
 app.use(errorHandler);
+app.use(express.static('public'));
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// app.listen(port, () => console.log(`Server started on port ${port}`));
+app.listen(port, () => {
+    console.log(`App listening on port ${port}!`)
+   });
+   
